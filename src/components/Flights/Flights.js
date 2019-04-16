79ec8b9a73ee4441200f0b9faaec397e08355bbb
finnishfox <onefinnishfox@gmail.com>
2019-04-16 16:59:13 +0200

    icons added

--- src/components/Flights/Flights.js
+++ src/components/Flights/Flights.js
@@ -181,30 +181,86 @@ class Flights extends React.Component {
                     <button
                         className={this.state.currentTab === 'departures' ? 'Flights__button Flights__button--active' : 'Flights__button'}
                         onClick={() => this.changeCurrentTab('departures')}>
+                        <svg className="Flights__departure-icon"
+                             viewBox="0 0 40 28"
+                             version="1.1"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                <g transform="translate(-1.000000, -1.000000)" fill="#FFFFFF" fillRule="nonzero"
+                                   className="Flights__plane-icon">
+                                    <g id="Group">
+                                        <path
+                                            d="M40.9190312,14.2925525 C40.731055,13.5210761 40.2396434,12.8581323 39.5561585,12.4539552 C38.8726735,12.0497781 38.0549837,11.9385881 37.2883912,12.1455827 L26.8096581,15.0218819 L13.1945082,2 L9.38678826,3.03046545 L17.5564785,17.5510243 L7.75204986,20.2442408 L3.86129243,17.1278331 L1,17.9171897 L4.59162231,24.3200818 L6.11030829,27.0112974 L9.26373268,26.1409043 L19.7414654,23.2656055 L28.3253427,20.9165445 L38.8070772,18.0372439 C40.4055057,17.5625364 41.3397513,15.9060318 40.9190312,14.2925525 Z"
+                                            id="Path_403"
+                                            transform="translate(21.009879, 14.505649) rotate(-4.012171) translate(-21.009879, -14.505649) "></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
                         Departures
                     </button>
                     <button
                         className={this.state.currentTab === 'arrivals' ? 'Flights__button Flights__button--active' : 'Flights__button'}
                         onClick={() => this.changeCurrentTab('arrivals')}>
+                        <svg className="Flights__departure-icon"
+                             viewBox="0 0 40 28"
+                             version="1.1"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                <g transform="translate(-1.000000, -1.000000)" fill="#FFFFFF" fillRule="nonzero"
+                                   className="Flights__plane-icon">
+                                    <g id="Group">
+                                        <path
+                                            d="M40.9190312,14.2925525 C40.731055,13.5210761 40.2396434,12.8581323 39.5561585,12.4539552 C38.8726735,12.0497781 38.0549837,11.9385881 37.2883912,12.1455827 L26.8096581,15.0218819 L13.1945082,2 L9.38678826,3.03046545 L17.5564785,17.5510243 L7.75204986,20.2442408 L3.86129243,17.1278331 L1,17.9171897 L4.59162231,24.3200818 L6.11030829,27.0112974 L9.26373268,26.1409043 L19.7414654,23.2656055 L28.3253427,20.9165445 L38.8070772,18.0372439 C40.4055057,17.5625364 41.3397513,15.9060318 40.9190312,14.2925525 Z"
+                                            id="Path_403"
+                                            transform="translate(21.009879, 14.505649) rotate(-4.012171) translate(-21.009879, -14.505649) "></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
                         Arrivals
                     </button>
                 </nav>
                 <nav className="Flights__dates-navigation">
-                    <button className="Flights__change-date-button"
-                            onClick={() => this.setState({currentDate: yesterday})}>
-                        <span className="Flights__date-text">{yesterday.format('DD/MM')}</span>
-                        Yesterday
-                    </button>
-                    <button className="Flights__change-date-button"
-                            onClick={() => this.setState({currentDate: moment()})}>
-                        <span className="Flights__date-text">{moment().format('DD/MM')}</span>
-                        Today
-                    </button>
-                    <button className="Flights__change-date-button"
-                            onClick={() => this.setState({currentDate: tomorrow})}>
-                        <span className="Flights__date-text">{tomorrow.format('DD/MM')}</span>
-                        Tomorrow
-                    </button>
+                    <div className="Flights__calendar">
+                        <p className="Flights__current-date">{moment(this.state.currentDate).format('DD/MM')}</p>
+                        <div className="Flights__calendar-wrapper">
+                            <svg className="Flights__calendar-icon"
+                                 viewBox="0 0 96 86" version="1.1"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                    <g id="calendar" fill="#ffffff" fillRule="nonzero">
+                                        <path
+                                            d="M85.5,7.4 L75.5,7.4 L75.5,11.4 C75.5,14.9 72.6,17.8 69.1,17.8 C65.6,17.8 62.7,15 62.7,11.4 L62.7,7.4 L33.2,7.4 L33.2,11.4 C33.2,14.9 30.4,17.8 26.8,17.8 C23.3,17.8 20.4,15 20.4,11.4 L20.4,7.4 L10.4,7.4 C4.9,7.4 0.4,11.9 0.4,17.4 L0.4,75.6 C0.4,81.1 4.9,85.6 10.4,85.6 L85.4,85.6 C90.9,85.6 95.4,81.1 95.4,75.6 L95.4,17.4 C95.5,11.9 91,7.4 85.5,7.4 Z M90,75.6 C90,78.1 88,80.1 85.5,80.1 L10.5,80.1 C8,80.1 6,78.1 6,75.6 L6,29.3 L90,29.3 L90,75.6 Z"
+                                            id="Shape"></path>
+                                        <path
+                                            d="M26.9,14.2 C28.4,14.2 29.6,13 29.6,11.5 L29.6,3.2 C29.6,1.7 28.4,0.5 26.9,0.5 C25.4,0.5 24.2,1.7 24.2,3.2 L24.2,11.5 C24.1,13 25.4,14.2 26.9,14.2 Z"
+                                            id="Shape"></path>
+                                        <path
+                                            d="M69.1,14.2 C70.6,14.2 71.8,13 71.8,11.5 L71.8,3.2 C71.8,1.7 70.6,0.5 69.1,0.5 C67.6,0.5 66.4,1.7 66.4,3.2 L66.4,11.5 C66.4,13 67.6,14.2 69.1,14.2 Z"
+                                            id="Shape"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="Flights__navigation-wrapper">
+                        <button className="Flights__change-date-button"
+                                onClick={() => this.setState({currentDate: yesterday})}>
+                            <span className="Flights__date-text">{yesterday.format('DD/MM')}</span>
+                            Yesterday
+                        </button>
+                        <button className="Flights__change-date-button"
+                                onClick={() => this.setState({currentDate: moment()})}>
+                            <span className="Flights__date-text">{moment().format('DD/MM')}</span>
+                            Today
+                        </button>
+                        <button className="Flights__change-date-button"
+                                onClick={() => this.setState({currentDate: tomorrow})}>
+                            <span className="Flights__date-text">{tomorrow.format('DD/MM')}</span>
+                            Tomorrow
+                        </button>
+                    </div>
                 </nav>
                 <table className="Flights__table">
                     <thead>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,12 +2,12 @@ import React from "react";
 import './Search.scss';
 
 class Search extends React.Component {
-    state = {searchQuery:''};
+    state = {searchQuery: ''};
 
-    search = (event)=>{
+    search = (event) => {
         event.preventDefault();
         const {searchQuerySet} = this.props;
-        searchQuerySet({searchQuery:this.state.searchQuery});
+        searchQuerySet({searchQuery: this.state.searchQuery});
     };
 
     render() {
@@ -16,11 +16,22 @@ class Search extends React.Component {
                 <h2 className="Search__title">SEARCH FLIGHT</h2>
                 <form className="Search__form">
                     <label className="Search__field">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-512.053 29 44 43" className="Search__icon">
+                            <g id="ic_search_white" transform="translate(-512.053 29)">
+                                <rect width="44"
+                                      height="43"
+                                      fill="none"></rect>
+                                <path
+                                    d="M317.717,70.708H316.25l-.55-.538a11.072,11.072,0,0,0,2.933-7.525,11.92,11.92,0,1,0-11.917,11.646,11.628,11.628,0,0,0,7.7-2.867l.55.538V73.4l9.167,8.958,2.75-2.687Zm-11,0a8.065,8.065,0,1,1,8.25-8.063A8.124,8.124,0,0,1,306.717,70.708Z"
+                                    transform="translate(-289.3 -45.625)"
+                                    fill="#95989A"></path>
+                            </g>
+                        </svg>
                         <input
                             type="text"
                             placeholder="Destination or flight #"
                             className="Search__input"
-                        onChange={(event)=>this.setState({searchQuery:event.target.value})}/>
+                            onChange={(event) => this.setState({searchQuery: event.target.value})}/>
                     </label>
                     <button
                         type="submit"
