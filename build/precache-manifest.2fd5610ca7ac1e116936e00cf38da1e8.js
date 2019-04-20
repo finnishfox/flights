84324b0ed701fe2b37afed81378bcaf6a34160b3
finnishfox <onefinnishfox@gmail.com>
2019-04-20 18:11:11 +0200

    directory pathes fixed

---
+++ build/precache-manifest.2fd5610ca7ac1e116936e00cf38da1e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "03d6735a83dc87e1cc1c",
+    "url": "./static/js/main.673eeda2.chunk.js"
+  },
+  {
+    "revision": "9c39d1b148cfc5f21ca6",
+    "url": "./static/js/2.617ffff7.chunk.js"
+  },
+  {
+    "revision": "03d6735a83dc87e1cc1c",
+    "url": "./static/css/main.9bbb743c.chunk.css"
+  },
+  {
+    "revision": "9c39d1b148cfc5f21ca6",
+    "url": "./static/css/2.8aa93e38.chunk.css"
+  },
+  {
+    "revision": "e1727d3921eadd99dccdbc0fcc942dfc",
+    "url": "./index.html"
+  }
+];
