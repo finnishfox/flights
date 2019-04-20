38a7e04f6daf374c76f436abb696906029aca28d
finnishfox <onefinnishfox@gmail.com>
2019-04-20 17:49:55 +0200

    deploy to gh-pages

---
+++ precache-manifest.2fa7706a55618509e9bd29eab318f9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03d6735a83dc87e1cc1c",
+    "url": "/static/js/main.673eeda2.chunk.js"
+  },
+  {
+    "revision": "9c39d1b148cfc5f21ca6",
+    "url": "/static/js/2.617ffff7.chunk.js"
+  },
+  {
+    "revision": "03d6735a83dc87e1cc1c",
+    "url": "/static/css/main.9bbb743c.chunk.css"
+  },
+  {
+    "revision": "9c39d1b148cfc5f21ca6",
+    "url": "/static/css/2.8aa93e38.chunk.css"
+  },
+  {
+    "revision": "432ef56627f6cac0881c8f2d892633d2",
+    "url": "/index.html"
+  }
+];
